d693bf59aa86c9c7b0d3df3175631e002cf3ab7b
calexandredev <calexandredev@gmail.com>
2020-09-09 23:39:50 -0300

    configure schema and routes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/money-is-cash')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.general.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'"
+mongoose.Error.messages.general.Number.max = "O '{VALUE}' informado é menor que o limite minimo de '{MAX}'"
+mongoose.Error.messages.general.Number.enum = "O '{VALUE}' informado é menor que o limite minimo de '{ENUM}'"
