a5c3468a02b99b5ddac3ab6aacf9cd92adb1acef
calexandredev <calexandredev@gmail.com>
2020-09-17 21:33:28 -0300

    create menu and sideBar

--- frontend/src/main/app.js
+++ frontend/src/main/app.js
@@ -1,11 +1,13 @@
 import '../common/templates/dependencies'
 import React from 'react'
 import Header from '../common/templates/header'
+import SideBar from '../common/templates/SideBar'
 
 
 const App = props => (
     <div className='wrapper'>
       <Header />
+      <SideBar />
     </div>
 ) 
 
