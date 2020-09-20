24787045bf19c9ea43cd2b9a6b5d359d111f02cf
calexandredev <calexandredev@gmail.com>
2020-09-19 22:10:11 -0300

    create Footer

---
+++ frontend/src/common/templates/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = props => {
+  return (
+    <footer className='main-footer'>
+      <strong>
+        Copyright &copy; 2020
+        <a href='https://alexandresoueu.com.br' rel='noopener noreferrer' target='_blank'>
+            alexandresoueu
+        </a>.
+      </strong>
+    </footer>
+  )
+}
+
+export default Footer
--- frontend/src/main/app.js
+++ frontend/src/main/app.js
@@ -2,13 +2,17 @@ import '../common/templates/dependencies'
 import React from 'react'
 import Header from '../common/templates/header'
 import SideBar from '../common/templates/SideBar'
-
+import Footer from '../common/templates/Footer'
 
 const App = props => (
-    <div className='wrapper'>
-      <Header />
-      <SideBar />
+  <div className='wrapper'>
+    <Header />
+    <SideBar />
+    <div className='content-wrapper'>
+      <h1>Content</h1>
     </div>
-) 
+    <Footer />
+  </div>
+)
 
 export default App
