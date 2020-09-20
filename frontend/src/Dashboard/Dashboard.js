d6522f17a27c2e1fe2f17ce37d611cc5c69d243b
calexandredev <calexandredev@gmail.com>
2020-09-20 17:17:36 -0300

    create component Row and ValueBox

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import Row from '../common/layout/Row'
 import Content from '../common/templates/content'
 import ContentHeader from '../common/templates/contentHeader'
+import ValueBox from '../common/widget/ValueBox'
 
 class Dashboard extends Component {
   render () {
@@ -8,7 +10,11 @@ class Dashboard extends Component {
       <div>
         <ContentHeader title='Titulão' subTitle='titulinho' />
         <Content>
-          Dashboard
+          <Row>
+            <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Amount Credit' />
+            <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 20' text='Amount Debit' />
+            <ValueBox cols='12 4' color='blue' icon='money' value='R$ 30' text='Cash' />
+          </Row>
         </Content>
       </div>
     )
