9e9e23e346811e0c42d21ac24393c122f70f9cce
calexandredev <calexandredev@gmail.com>
2020-09-21 21:30:49 -0300

    install redux devtools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,8 @@ import promise from 'redux-promise'
 import App from './main/app'
 import reducers from '../src/main/reducers'
 
-const store = applyMiddleware(promise)(createStore)(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSIONS__ && window.__REDUX_DEVTOOLS_EXTENSIONS__()
+const store = applyMiddleware(promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
   <Provider store={store}>
