94eb3f2ffa969ad6db75f06bf46d55410a8b390a
calexandredev <calexandredev@gmail.com>
2020-10-05 21:21:44 -0300

    create get billing cycles with reducer and action

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import DashboardReducer from '../Dashboard/DashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../BillingCycle/BillingCycleReducer'
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
-  tab: TabReducer
+  tab: TabReducer,
+  billingCycle: BillingCycleReducer,
 })
 
 export default rootReducer
