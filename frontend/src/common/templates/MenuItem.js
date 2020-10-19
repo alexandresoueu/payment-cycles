2e913ba3135b63b3731a9128883d36818f226f38
calexandredev <calexandredev@gmail.com>
2020-10-18 23:24:07 -0300

    adjust menu responsive

--- frontend/src/common/templates/MenuItem.js
+++ frontend/src/common/templates/MenuItem.js
@@ -4,7 +4,8 @@ const MenuItem = props => {
   return (
     <li>
       <a href={props.path}>
-        <i className={`fa fa-${props.icon}`} /> {props.label}
+        <i className={`fa fa-${props.icon}`} />
+        <span>{props.label}</span>
       </a>
     </li>
   )
