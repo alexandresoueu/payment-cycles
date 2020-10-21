3c096904f598276bb9b10963cbce0c2e3649c378
calexandredev <calexandredev@gmail.com>
2020-10-21 19:20:23 -0300

    Login working adjusts on Backend

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const auth = require('auth')
+const auth = require('./auth')
 
 module.exports = function (server) {
 
@@ -15,7 +15,7 @@ module.exports = function (server) {
   const openApi = express.Router()
   server.use('/oapi', openApi)
   
-  const AuthService = require('../api/user/AuthService')
+  const AuthService = require('../api/user/authService')
   openApi.post('/login', AuthService.login)
   openApi.post('/signup', AuthService.signup)
   openApi.post('/validateToken', AuthService.validateToken)
